77ddc8958be3410da232116c4022907dbe54413e
Jason Gorst <jason.gorst@me.com>
2023-05-14 03:46:05 -0500

    add models

---
+++ lib/slash_commands/default.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+SlackRubyBotServer::Events.configure do |config|
+  config.on :command do |command|
+    command.logger.info "Received #{command[:command]}."
+    nil
+  end
+end
