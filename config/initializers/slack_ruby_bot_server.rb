4174dba25763306bfc00e85c3d9222c3caee4424
Jason Gorst <jason.gorst@me.com>
2023-06-15 23:13:25 -0500

    log to file

--- config/initializers/slack_ruby_bot_server.rb
+++ config/initializers/slack_ruby_bot_server.rb
@@ -3,4 +3,5 @@
 SlackRubyBotServer.configure do |config|
   config.oauth_version = :v2
   config.oauth_scope = %w[users:read channels:read channels:join groups:read chat:write commands incoming-webhook]
+  config.logger = Logger.new("log/#{ENV['RACK_ENV']}.log")
 end
